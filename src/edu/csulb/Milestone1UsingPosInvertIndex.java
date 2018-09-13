b52ff5e3af4cdc31791cc3ae627081a20679f4f7
Victoria Hong <victoria.hong@student.csulb.edu>
2018-09-13 14:13:31 -0700

    updated Milestone main to choose from different corpura

--- src/edu/csulb/Milestone1UsingPosInvertIndex.java
+++ src/edu/csulb/Milestone1UsingPosInvertIndex.java
@@ -11,6 +11,7 @@ import cecs429.text.BasicTokenProcessor;
 import cecs429.text.EnglishTokenStream;
 import cecs429.text.TokenProcessor;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Path;
@@ -19,30 +20,74 @@ import java.util.Scanner;
 
 public class Milestone1UsingPosInvertIndex
 {
-    public static void main(String[] args) throws IOException {
-        Path currentPath = Paths.get(System.getProperty("user.dir"));
-        Path corpusPath = Paths.get(currentPath.toString(), "corpora", "all-nps-sites");
-
-        DocumentCorpus corpus = DirectoryCorpus.loadTextDirectory(corpusPath, ".txt");
-        ((DirectoryCorpus) corpus).registerFileDocumentFactory(".json", JsonFileDocument::loadJsonFileDocument);
-        Index index = indexCorpus(corpus);
-
-
-        TokenProcessor processor = new BasicTokenProcessor();
+    public static void main(String[] args) throws IOException
+    {
         Scanner keyboard = new Scanner(System.in);
+        TokenProcessor processor = new BasicTokenProcessor();
 
-        System.out.print("Enter term to search (or \"quit\" to exit): ");
-        String word = processor.processToken(keyboard.next());
+        Path currentPath = Paths.get(System.getProperty("user.dir"));
+        Path corpusFolder = Paths.get(currentPath.toString(), "corpora");
 
-        while (!word.equals("quit"))
+        System.out.println("Directories");
+        System.out.println("------------------------------");
+        File[] files = new File(corpusFolder.toString()).listFiles();
+        for (int i = 0; i < files.length; i++)
         {
-            for (Posting p : index.getPostings(word)) {
-                //System.out.println("Document ID " + p.getDocumentId());
-                System.out.println("Document Title: " + corpus.getDocument(p.getDocumentId()).getTitle());
+            if (files[i].isDirectory())
+            {
+                System.out.println(i+1 + ": "+ files[i].getName());
             }
-            System.out.print("Enter term to search (or \"quit\" to exit): ");
-            word = processor.processToken(keyboard.next());
         }
+
+        System.out.print("\nEnter the number of the corpus to index (or \"quit\" to exit): ");
+        String dir = processor.processToken(keyboard.next());
+
+        while (!dir.equals("quit"))
+        {
+            try
+            {
+                if (Integer.parseInt(dir) <= files.length && Integer.parseInt(dir) > 0)
+                {
+                    currentPath = Paths.get(corpusFolder.toString(), files[Integer.parseInt(dir)-1].getName());
+                    DocumentCorpus corpus = DirectoryCorpus.loadTextDirectory(currentPath, ".txt");
+                    ((DirectoryCorpus) corpus).registerFileDocumentFactory(".json", JsonFileDocument::loadJsonFileDocument);
+                    Index index = indexCorpus(corpus);
+
+                    System.out.print("Enter term to search (or \"quit\" to exit): ");
+                    String word = processor.processToken(keyboard.next());
+
+                    while (!word.equals("quit"))
+                    {
+                        for (Posting p : index.getPostings(word))
+                        {
+                            //System.out.println("Document ID " + p.getDocumentId());
+                            System.out.println("Document Title: " + corpus.getDocument(p.getDocumentId()).getTitle());
+                        }
+                        System.out.print("Enter term to search (or \"quit\" to exit): ");
+                        word = processor.processToken(keyboard.next());
+                    }
+                    System.out.println("\nDirectories");
+                    System.out.println("------------------------------");
+                    for (int i = 0; i < files.length; i++)
+                    {
+                        if (files[i].isDirectory())
+                        {
+                            System.out.println(i+1 + ": "+ files[i].getName());
+                        }
+                    }
+
+                    System.out.print("\nEnter the number of the corpus to index (or \"quit\" to exit): ");
+                    dir = processor.processToken(keyboard.next());
+                }
+            }
+            catch (NumberFormatException e)
+            {
+                System.out.println("Please enter a number from 1-"+files.length+" (or \"quit\" to exit): ");
+                dir = processor.processToken(keyboard.next());
+            }
+        }
+
+
     }
 
     private static Index indexCorpus(DocumentCorpus corpus) throws IOException
